a0c0339fcf5fb10e090b9617febd07bdb1cf1b7c
Jean Parpaillon <jean.parpaillon@free.fr>
2019-03-04 15:56:44 +0100

    Add notation tests

--- test/ecto/fsm_notation_test.exs
+++ test/ecto/fsm_notation_test.exs
@@ -2,4 +2,19 @@ defmodule Ecto.FSM.NotationTest do
   use ExUnit.Case
 
   doctest Ecto.FSM.Notation
+
+  describe "FSM definition" do
+    test ".transition/2" do
+      {:module, mod, _, _} =
+        defmodule Elixir.Fsm1 do
+          use Ecto.FSM.Notation
+
+          transition init({:action, _}, s) do
+            {:next_state, :end, s}
+          end
+        end
+
+      assert match?(%{{:init, :action} => {Elixir.Fsm1, [:end]}}, mod.fsm())
+    end
+  end
 end
